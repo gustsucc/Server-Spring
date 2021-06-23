e5f807f8f1b46eab3c94c7aa915175e2815b4a38
gustsucc <gustsucc@gmail.com>
2021-06-23 10:25:16 -0400

    Usuario agregado por Noelia

---
+++ src/main/java/example/demo/Usuario.java
@@ -0,0 +1,74 @@
+package example.demo;
+
+//import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+//import javax.persistence.ManyToOne;
+//import javax.persistence.Table;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue
+    
+    private String login;
+    private String clave;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+   
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((login == null) ? 0 : login.hashCode());
+        result = prime * result + ((clave == null) ? 0 : clave.hashCode());
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (login == null) {
+            if (other.login != null)
+                return false;
+        } else if (!login.equals(other.login))
+            return false;
+        if (clave == null) {
+            if (other.clave != null)
+                return false;
+        } else if (!clave.equals(other.clave))
+            return false;
+        
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [clave=" + clave + ", login=" + login + "]";
+    }
+
+    public Usuario() {
+    }
+    
+}
