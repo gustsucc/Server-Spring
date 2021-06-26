8668b64eb0b27a9331a6be67e9c93613fb47ad9b
gustsucc <gustsucc@gmail.com>
2021-06-25 21:38:27 -0400

    Junio26

--- src/main/java/example/demo/RepoEstudiantes.java
+++ src/main/java/example/demo/RepoEstudiantes.java
@@ -2,7 +2,6 @@ package example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 @RepositoryRestResource
 public interface RepoEstudiantes extends CrudRepository<Estudiante,Long> {
