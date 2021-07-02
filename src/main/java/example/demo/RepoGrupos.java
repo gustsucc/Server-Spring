f0d9caca14b59b0c6e4f02e5f39e8482748d249a
gustsucc <gustsucc@gmail.com>
2021-07-02 08:21:18 -0400

    Julio2

--- src/main/java/example/demo/RepoGrupos.java
+++ src/main/java/example/demo/RepoGrupos.java
@@ -3,7 +3,7 @@ package example.demo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "grupos")
 public interface RepoGrupos extends CrudRepository<Grupo,Long> {
     
 }
