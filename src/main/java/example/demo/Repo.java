e1b8ddee570b5d5597c3275ac16d7fe04c5f1ce9
gustsucc <gustsucc@gmail.com>
2021-07-25 07:28:34 -0400

    Acceso Cross Origin

--- src/main/java/example/demo/Repo.java
+++ src/main/java/example/demo/Repo.java
@@ -2,8 +2,10 @@ package example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin("*")
 public interface Repo extends CrudRepository<Docente,Long> {
 
     Iterable<Docente> findByEmail(String string);
